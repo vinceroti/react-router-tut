971a9ed25c92d9cd15929733d71ef1690a8cf2ad
Vince Roti <vince.roti@gmail.com>
2017-03-17 21:21:10 -0500

    add NavLink component to make our code more modular

--- lessons/05-active-links/modules/App.js
+++ lessons/05-active-links/modules/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router'
+import NavLink from './NavLink'
 
 export default React.createClass({
   render() {
@@ -7,11 +8,13 @@ export default React.createClass({
       <div>
         <h1>React Router Tutorial</h1>
         <ul role="nav">
-          <li><Link to="/about" activeClassName="active">About</Link></li>
-          <li><Link to="/repos" activeClassName="active">Repos</Link></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/repos">Repos</NavLink></li>
         </ul>
         {this.props.children}
       </div>
     )
   }
 })
+
+//navlink is our custom link componenent so we don't have to tell it what the active class is everytime, shortens code and makes it more modular
