095081b22e71e0d908468ff167d6790804b1b503
Vince Roti <vince.roti@gmail.com>
2017-03-20 13:06:23 -0500

    adds compression and minifie

--- lessons/11-productionish-server/webpack.config.js
+++ lessons/11-productionish-server/webpack.config.js
@@ -1,7 +1,10 @@
+var webpack = require('webpack')
+
 module.exports = {
   entry: './index.js',
 
   output: {
+    path: 'public',
     filename: 'bundle.js',
     publicPath: '/'
   },
@@ -10,5 +13,11 @@ module.exports = {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
     ]
-  }
+  },
+
+  plugins: process.env.NODE_ENV === 'production' ? [
+     new webpack.optimize.DedupePlugin(),
+     new webpack.optimize.OccurrenceOrderPlugin(),
+     new webpack.optimize.UglifyJsPlugin()
+   ] : [],
 }
