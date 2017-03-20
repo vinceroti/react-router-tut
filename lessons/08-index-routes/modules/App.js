c8c9ba3fcd42e57f71aaeb22e291df89d2e4741b
Vince Roti <vince.roti@gmail.com>
2017-03-19 19:50:34 -0500

    proper linking to index

--- lessons/08-index-routes/modules/App.js
+++ lessons/08-index-routes/modules/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { IndexLink } from 'react-router'
 import NavLink from './NavLink'
 
 export default React.createClass({
@@ -7,6 +8,8 @@ export default React.createClass({
       <div>
         <h1>React Router Tutorial</h1>
         <ul role="nav">
+          <li><NavLink to="/" onlyActiveOnIndex={true}>Home</NavLink></li>
+        {/*<li><IndexLink to="/" activeClassName="active">Home</IndexLink></li> both ways allow index linking*/}
           <li><NavLink to="/about">About</NavLink></li>
           <li><NavLink to="/repos">Repos</NavLink></li>
         </ul>
