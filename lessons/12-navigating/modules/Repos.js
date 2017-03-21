250343cac23cdb6296a48ad221f55d247697c563
Vince Roti <vince.roti@gmail.com>
2017-03-20 20:25:25 -0500

    adds navigation

--- lessons/12-navigating/modules/Repos.js
+++ lessons/12-navigating/modules/Repos.js
@@ -1,7 +1,24 @@
 import React from 'react'
 import NavLink from './NavLink'
+import { browserHistory } from 'react-router'
 
 export default React.createClass({
+
+  contextTypes: {
+     router: React.PropTypes.object
+   },
+
+   //You can also use the router that Router provides on "context". First, you ask for context in the component, and then you can use it:
+
+  // add this method
+  handleSubmit(event) {
+    event.preventDefault()
+    const userName = event.target.elements[0].value
+    const repo = event.target.elements[1].value
+    const path = `/repos/${userName}/${repo}`
+    this.context.router.push(path)
+  },
+
   render() {
     return (
       <div>
@@ -9,6 +26,14 @@ export default React.createClass({
         <ul>
           <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
           <li><NavLink to="/repos/facebook/react">React</NavLink></li>
+          {/* add this form */}
+          <li>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" placeholder="userName"/> / {' '}
+              <input type="text" placeholder="repo"/>{' '}
+              <button type="submit">Go</button>
+            </form>
+          </li>
         </ul>
         {this.props.children}
       </div>
